dcf14c5f790d899591c86acc4d4a8a6483946eaa
ifournight <ifournight@gmail.com>
2017-02-24 00:13:49 +0800

    add product, basic style for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+    {title: 'BOSE QuietComfort 35', description: '静情聆听 细致心跳', image_url: 'https://making-photos.b0.upaiyun.com/photos/2b85518b63baa3bd4627e86d339b2f89.jpg!middle', price: 399.99},
+    {title: 'Nespresso Gran Maestria XN8105', description: '雀巢家用全自动胶囊咖啡机', image_url: 'https://making-photos.b0.upaiyun.com/photos/4ff4e4546a046b24c0f76161a1275b5f.jpg!middle', price: 499},
+])
